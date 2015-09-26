42461b8384d7fd183c8b1c89d3c7bbd68c9fce91
Ralf <xoodrenalin@gmx.de>
2015-09-26 22:47:54 +0200

    working a1-4

--- web/a1.php
+++ web/a1.php
@@ -12,47 +12,96 @@
     <script type="text/javascript" src="../resources/jquery-1.11.3.min.js"></script>
 
     <script>
-        $(document)
-            .ready(function(){
+        // buttons
+        $(document).ready(
+            function() {
                 $("#sendFields")
-                    .click(function(){
+                    .click(
+                    function () {
                         $.ajax({
-                            url: "getLoggerJSON.php",
-                            success: function(result){
-
-                                console.log(result);
-
-                                if( result != null && result.length > 0) {
-                                    var parse = JSON.parse(result);
-
-                                    var i;
-                                    var content = '';
-                                    for (i = 0; i < parse.length; i++) {
-                                        content += parse[i].id + ":" + parse[i].message;
-                                        content += '<br />'
-                                    }
-                                }
-
-                                $("#result").html(content);
-                            },
-                            error: function(result){
-                                alert(result);
+                            type: "POST",
+                            url: "logger.php",
+                            data: {id: $("#eingabeMsg").val(), msg: $("#eingabeId").val()},
+                            success: function (data) {
+                                console.log("send data for sendFields");
                             }
                         });
-                    });
+                    })
             });
+
+        $(document).ready(
+            function() {
+                $("#sendDefault")
+                    .click(
+                    function () {
+                        $.ajax({
+                            type: "POST",
+                            url: "logger.php",
+                            data: {id: "otto", msg: "eine nachricht"},
+                            success: function (data) {
+                                console.log("send data for sendDefault");
+                            }
+                        });
+                    })
+            });
+
+        $(document).ready(
+            function() {
+                $("#btnReset")
+                    .click(
+                    function () {
+                        $.ajax({
+                            type: "POST",
+                            url: "resetlog.php",
+                            data: {},
+                            success: function (data) {
+                                console.log("send data for sendDefault");
+                            }
+                        });
+                    })
+            });
+
+        function readLog(){
+            $.ajax({
+                url: "getLoggerJSON.php",
+                success: function(result){
+
+                    console.log(result);
+
+                    if( result != null && result.length > 0) {
+                        var parse = JSON.parse(result);
+
+                        var i;
+                        var content = '';
+                        for (i = 0; i < parse.length; i++) {
+                            content += parse[i].id + ":" + parse[i].message;
+                            content += '<br />'
+                        }
+                    }
+
+                    $("#result").html(content);
+                },
+                error: function(result){
+                    alert(result);
+                }
+            });
+        }
+
+        // polling
+        setInterval(readLog, 1000);
     </script>
 </head>
 <body>
 <p>Eingabe:</p>
-<textarea id="eingabeId" >eineId</textarea>
-<textarea id="eingabeMsg" >hallo jan</textarea>
+<label for="eingabeId" >ID:</label>
+<input type="text" id="eingabeId" value="eineId" />
+<label for="eingabeMsg" >Msg:</label>
+<input type="text" id="eingabeMsg" value="hallo jan" />
 <br/>
 <button id="sendFields" >senden feld</button>
-<!--
-<button onclick="log('otto', 'standard');">senden standard</button>
-<button onclick="resetlog(); document.getElementById('Comment').innerHTML='';">reset</button>
--->
+<button id="sendDefault" >senden default</button>
+
+<button id="btnReset" >reset</button>
 
 <br />
 <p>Ausgabe:</p>
@@ -63,9 +112,6 @@
 <button onclick="getLog();">lesen</button>
 -->
 
-<script type="application/javascript">
-    // setInterval(function(){getLog();}, 1000);
-</script>
 
 </body>
 </html>
